1a1e70d7c5d3d4ad188266be5ccbf87c1f4a3b54
Rimanas <rimanas_alina@mail.ru>
2020-11-12 19:14:43 +0300

    Fixed description of columns in the table friends-user_one,user_two and in the table messages-sender,recipient, status

--- resources/schema.sql
+++ resources/schema.sql
@@ -24,4 +24,32 @@ CREATE TABLE messages
     sender INTEGER REFERENCES users,
     recipient INTEGER REFERENCES users,
     status INTEGER REFERENCES status
-)
+);
+
+ALTER TABLE friends RENAME TO fr;
+CREATE TABLE friends
+(
+    id INTEGER PRIMARY KEY AUTOINCREMENT ,
+    user_one INTEGER NOT NULL REFERENCES users,
+    user_two INTEGER NOT NULL REFERENCES users
+);
+
+INSERT INTO friends(user_one, user_two)
+SELECT user_one, user_two
+FROM fr;
+
+DROP TABLE fr;
+
+ALTER TABLE messages RENAME TO ms;
+CREATE TABLE messages
+(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    text TEXT NOT NULL ,
+    sender INTEGER NOT NULL REFERENCES users,
+    recipient INTEGER NOT NULL REFERENCES users,
+    status INTEGER NOT NULL REFERENCES status
+);
+INSERT INTO messages(text, sender, recipient, status)
+SELECT text, sender, recipient, status
+FROM ms;
+DROP TABLE ms;
